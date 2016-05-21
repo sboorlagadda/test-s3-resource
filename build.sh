52339cffe993b74b31da0a82401d14f151835b42
Sai Boorlagadda <sboorlagadda@pivotal.io>
2016-05-20 22:56:37 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export TERM=${TERM:-dumb}
+
+set -e
+
+VERSION=$(cat apigateway-version/version)
+
+pushd test-s3-resource
+echo $VERSION >> build-$VERSION.jar
+popd
+cp test-s3-resource/build-*.jar builds
